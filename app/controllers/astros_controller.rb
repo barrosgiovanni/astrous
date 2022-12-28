9c560469b3ca31e76d5df80ead9b3d2aaaeab6d2
Giovanni Barros <barrosgiovanni@icloud.com>
2022-12-28 15:58:01 -0300

    added #show action on controller, created show view and gave it a bit of style

--- app/controllers/astros_controller.rb
+++ app/controllers/astros_controller.rb
@@ -1,9 +1,20 @@
 class AstrosController < ApplicationController
+  before_action :set_astro, only: [:show, :destroy]
+
   def index
     if params[:query].present?
       @astros = Astro.search_astros(params[:query])
     else
       @astros = Astro.all
     end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_astro
+    @astro = Astro.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :astros
+  resources :astros do
+    resources :bookings
+  end
 end
