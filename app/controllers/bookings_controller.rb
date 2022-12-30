40493253187ce9f7019d4e48d6d248bc572b16f4
Giovanni Barros <barrosgiovanni@icloud.com>
2022-12-30 18:06:18 -0300

    added new and create actions to bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,34 @@
 class BookingsController < ApplicationController
+  before_action :set_astro_id, only: [:new, :create]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.astro = @astro
+    @booking.user = current_user
+    @booking.status = "Pending."
+
+    if @booking.save
+      redirect_to astro_bookings_path, notice: "Booking request was succesfully created! Check its status!"
+    else
+      render :new, notice: "Unfortunately, it's not possible to proceed with your request."
+    end
+  end
+
+  private
+
+  def set_astro_id
+    @astro = Astro.find(params[:astro_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:check_in, :check_out, :status)
+  end
 end
