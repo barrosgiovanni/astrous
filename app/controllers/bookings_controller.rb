1504b914b5d60391dfae9390988c406e2eb47712
Giovanni Barros <barrosgiovanni@icloud.com>
2022-12-31 10:07:29 -0300

    adjustments to bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
-  before_action :set_astro_id, only: [:new, :create]
-  before_action :set_booking, only: [:confirm, :reject]
+  before_action :set_astro_id, only: [:new, :create, :confirm, :reject]
+  before_action :set_booking, only: [:confirm, :reject, :destroy]
 
   def index
     @bookings = Booking.where("user_id = ?", current_user.id)
@@ -16,17 +16,16 @@ class BookingsController < ApplicationController
     @booking.user = current_user
     @booking.status = "Pending"
 
-    if @booking.save
-      redirect_to astro_bookings_path, notice: "Booking request was succesfully created! Check its status!"
+    if @booking.save!
+      redirect_to bookings_path, notice: "Booking request was succesfully created! Check its status!"
     else
       render :new, notice: "Unfortunately, it's not possible to proceed with your request."
     end
   end
 
   def destroy
-    @booking = Booking.find(params[:id])
     @booking.destroy
-    redirect_to astro_bookings_path, notice: "Booking was successfully cancelled!"
+    redirect_to astro_path(@booking.astro), notice: "Booking was successfully cancelled!"
   end
 
   def astros_host
@@ -34,13 +33,13 @@ class BookingsController < ApplicationController
   end
 
   def confirm
-    booking.confirm!
-    redirect_to astros_host_bookings_path, notice: "Your booking request was confirmed!"
+    @booking.confirm!
+    redirect_to astros_host_astro_bookings_path, notice: "Your booking request was confirmed!"
   end
 
   def reject
-    booking.reject!
-    redirect_to astros_host_bookings_path, notice: "Your booking request was rejected!"
+    @booking.reject!
+    redirect_to astros_host_astro_bookings_path, notice: "Your booking request was rejected!"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :astros do
-    resources :bookings, only: %i[new create index destroy] do
+  resources :astros, only: %i[home index show new create edit update destroy] do
+    resources :bookings, only: %i[new create]
       collection do
         get :list_astros
-        get :astros_host
       end
+  end
+
+  resources :bookings, only: %i[index destroy] do
+    collection do
+      get :astros_host
     end
   end
 
