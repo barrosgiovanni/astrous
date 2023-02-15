d202df1260f2751bece95d5015e6b14e0360f6df
Giovanni Barros <barrosgiovanni@icloud.com>
2023-02-15 00:52:13 -0300

    added update, delete and index policies

--- app/policies/astro_policy.rb
+++ app/policies/astro_policy.rb
@@ -1,16 +1,32 @@
 class AstroPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
   end
 
   def show?
     true
   end
 
+  def new?
+    create?
+  end
+
   def create?
     true
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
   end
 end
